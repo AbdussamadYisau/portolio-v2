0311e1259cad3c528396189f35ad28a6fc5906bd
AbdussamadYisau <sammieyisau@gmail.com>
2022-10-23 13:13:59 +0100

    Added typing animation

--- components/Bio.js
+++ components/Bio.js
@@ -1,6 +1,29 @@
 import Image from "next/image";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 export default function Bio() {
+     // Create Ref element.
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Yisau Abdussamad"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      showCursor: false,
+      
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-row justify-start gap-8 border-b pb-5 border-[#8C8C8C] dark:border-white dark:border-opacity-[.12] pt-14">
@@ -14,7 +37,8 @@ export default function Bio() {
         </div>
 
         <div className="pt-8">
-          <h1 className="text-6xl dark:text-white">{`<Yisau Abdussamad>`}</h1>
+          {/* <h1 className="text-6xl dark:text-white">{`<Yisau Abdussamad>`}</h1> */}
+          <h1 className="text-6xl dark:text-white" ref={el}></h1>
           <p className="text-2xl text-[#8C8C8C]">//fullstack developer</p>
         </div>
       </div>
