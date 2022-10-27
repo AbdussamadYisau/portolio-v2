59c4ece52efc9ac43c048fa0d24bc089a8c097be
AbdussamadYisau <sammieyisau@gmail.com>
2022-10-27 22:36:21 +0100

    made tweaks to how credit looks on mobile

--- components/Credit.js
+++ components/Credit.js
@@ -1,8 +1,12 @@
 export default function Credit() {
     return (
-      <div className="flex flex-row justify-center gap-x-4 mb-[40px]">
+      <div className="flex w-full flex-col md:flex-row justify-center md:gap-x-4 md:items-center mb-[40px]">
         <p className="text-[#595959]">designed by oluwapelumi abimbola</p>
-        <p className="text-[#595959]">|</p>
+        <p className="text-[#595959] hidden md:inline">|</p>
+        <div className="md:hidden flex flex-row justify-center">
+        <p className="w-1/2 text-center  border-b mb-[4px] border-[#8C8C8C] dark:border-white dark:border-opacity-[.12] mt-[4px]"></p>
+        </div>
+       
         <p className="text-[#595959]">developed by yisau abdussamad</p>
       </div>
     );
